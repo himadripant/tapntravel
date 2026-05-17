920f69aee46287699c35c6c927668cdaf469a623
Himadri Pant <himadripant@gmail.com>
2026-05-18 01:01:24 +1000

    updated fare computation and related test cases

--- src/main/java/hp/tasks/tapntravel/repositories/TapRepository.java
+++ src/main/java/hp/tasks/tapntravel/repositories/TapRepository.java
@@ -2,7 +2,6 @@ package hp.tasks.tapntravel.repositories;
 
 import hp.tasks.tapntravel.entities.Tap;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.math.BigInteger;
@@ -10,7 +9,6 @@ import java.util.List;
 
 public interface TapRepository extends JpaRepository<Tap, BigInteger> {
 
-    @Query("SELECT t FROM Tap t JOIN FETCH t.beginStop LEFT JOIN FETCH t.endStop WHERE t.pan = :pan AND t.busId = :busId AND t.busCompanyId = :busCompanyId")
     List<Tap> findByPanAndBusIdAndBusCompanyId(
             @Param("pan") String pan, @Param("busId") String busId, @Param("busCompanyId") Integer busCompanyId
     );
